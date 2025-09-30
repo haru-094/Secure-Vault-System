295fb9634984ba579a703ef5ff20ca4a5d258ccc
haru-094 <a.nasir@alustudent.com>
2025-09-30 00:56:17 -0700

    fix error in step 3

--- vault_setup.sh
+++ vault_setup.sh
@@ -5,7 +5,7 @@ mkdir -p "$secure_dir"
 
 echo "Welcome to your secure vault store your encrption here" > "$secure_dir/keys.txt"
 echo "Welcome to your secure vault stores confidential data here" > "$secure_dir/secrets.txt"
-echo "Welcome to your secure vault stores system logs here" > "$secure/logs.txt"
+echo "Welcome to your secure vault stores system logs here" > "$secure_dir/logs.txt"
 
 echo "Vault created"
 ls -l "$secure_dir"
